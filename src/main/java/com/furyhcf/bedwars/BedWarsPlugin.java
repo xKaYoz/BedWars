c6d83e28a3f9f57e3ffffc9185ba768134a378bc
xKaYoz <austinwvu2013@gmail.com>
2017-07-07 21:10:10 -0400

    Initial commit

---
+++ src/main/java/com/furyhcf/bedwars/BedWarsPlugin.java
@@ -0,0 +1,17 @@
+package com.furyhcf.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BedWarsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/furyhcf/bedwars/utils/ChatUtils.java
@@ -0,0 +1,16 @@
+package com.furyhcf.bedwars.utils;
+
+import org.bukkit.ChatColor;
+
+/**
+ * Created by KaYoz on 7/7/2017.
+ * Subscribe to me on Youtube:
+ * http://www.youtube.com/c/KaYozMC/
+ */
+
+public class ChatUtils {
+
+    public static String format(String msg){
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+}
